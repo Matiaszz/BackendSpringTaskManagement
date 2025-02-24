8e807a8a2934382d5219a6d48b3192211221235d
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2025-02-24 19:25:27 -0300

    initial configurations

--- TaskManagement/src/main/java/dev/matias/TaskManagement/TaskManagementApplication.java
+++ TaskManagement/src/main/java/dev/matias/TaskManagement/TaskManagementApplication.java
@@ -1,13 +1,17 @@
 package dev.matias.TaskManagement;
 
+import dev.matias.TaskManagement.utils.Utils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class TaskManagementApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		SpringApplication.run(TaskManagementApplication.class, args);
+		Utils.clearScreen();
 	}
 
 }
