8cfacaa49728f85935a00d9eac4ef7021d020f0c
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2025-02-24 16:27:26 -0300

    init

---
+++ TaskManagement/src/test/java/dev/matias/TaskManagement/TaskManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.matias.TaskManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
