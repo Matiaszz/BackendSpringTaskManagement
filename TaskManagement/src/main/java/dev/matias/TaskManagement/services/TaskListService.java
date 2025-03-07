e4254e0ee1485b4914dd23e24abcf5ee085e7470
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2025-03-07 13:21:44 -0300

    FEAT: Now, only the owner of the tasklist can change the specified tasklis

--- TaskManagement/src/main/java/dev/matias/TaskManagement/services/TaskListService.java
+++ TaskManagement/src/main/java/dev/matias/TaskManagement/services/TaskListService.java
@@ -6,7 +6,11 @@ import dev.matias.TaskManagement.dtos.TaskListDTO;
 import dev.matias.TaskManagement.dtos.TaskListUpdateDTO;
 import dev.matias.TaskManagement.repositories.TaskListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,6 +22,10 @@ public class TaskListService {
     private TaskListRepository taskListRepository;
 
     private MaxTaskDTO maxTaskDTO;
+
+    @Autowired
+    private AuthorizationService authorizationService;
+
 
     public List<TaskListDTO> getTasks() {
         List<TaskList> taskLists = taskListRepository.findAll();
@@ -60,6 +68,12 @@ public class TaskListService {
     }
 
     public TaskListDTO updateTaskList(TaskList taskList, TaskListUpdateDTO taskListUpdateDTO){
+        UserDetails loggedUser = authorizationService.getLoggedUser();
+
+        if (!taskList.getOwner().getUsername().equalsIgnoreCase(loggedUser.getUsername())){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "TaskList not found.");
+        }
+
         taskList.setTitle((taskListUpdateDTO.title() != null) ? taskListUpdateDTO.title() : taskList.getTitle());
 
         taskList.setShortDescription((
