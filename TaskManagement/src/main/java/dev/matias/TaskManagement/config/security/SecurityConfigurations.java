63655f7dd55d64fcf1e1f69b0568ce00e5584b12
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2025-03-15 11:02:25 -0300

    fix: CORS of frontend allowed

--- TaskManagement/src/main/java/dev/matias/TaskManagement/config/security/SecurityConfigurations.java
+++ TaskManagement/src/main/java/dev/matias/TaskManagement/config/security/SecurityConfigurations.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -22,9 +23,13 @@ public class SecurityConfigurations {
     @Autowired
     private SecurityFilter securityFilter;
 
+    @Autowired
+    private CorsConfigurationSource corsConfigurationSource;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
                 .csrf(AbstractHttpConfigurer::disable)
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement(session ->
